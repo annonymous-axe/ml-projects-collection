e4bdde039ba4884a476f70f0dad242abb6f41f0b
@KamleshBaviskar <79747679+annonymous-axe@users.noreply.github.com>
2023-12-04 19:25:53 +0530

    Adding pubg model...

---
+++ pubg-module/interface.py
@@ -0,0 +1,9 @@
+# import streamlit as st
+
+# st.header("PUBG Game Prediction")
+
+# st.number_input("Total Headshot kills", min_value=0, max_value=10, step=1, value=0)
+# st.number_input("Number of time Heals", min_value=0, max_value=80, step=1, value=0)
+# # kill place
+
+
